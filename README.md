022e2b080bc6c196f6a1ebf0b7704c074b1cdc90
BrandonKi <kirincichb@gmail.com>
2025-03-30 17:33:53 -0700

    compress backup

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # gh-local-backup
+
+The first step is downloading the github CLI app.
+Installation instructions for each OS can be found here [link](https://github.com/cli/cli?tab=readme-ov-file#installation).
+
+After installing, all you need to do is run the following:
+```
+python3 main.py
+```
+
+This will prompt you to login the first time you run it.
+Alternatively you can login beforehand by running:
+```
+gh auth login
+```
+
+You can check what accounts you are logged in with:
+```
+gh auth status
+```
+
+This tool will automatically create a gh_backup directory in the current folder and clone all your public/private repos into a subdirectory. It will not clone forks by default.
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from multiprocessing import Pool
 from datetime import datetime
 from functools import partial
-from tqdm import tqdm
+import shutil
 
 def run(command):
     return subprocess.run(command, shell=True, capture_output=True, text=True)
@@ -59,14 +59,14 @@ def login():
 
 def repo_list():
     # gh repo list USER_OR_ORG --json name,owner,url
-    res = run(["gh","repo","list", "-L", "5000", "--source", "--json", "name,url"])
+    res = run(["gh","repo","list", "-L", "4", "--source", "--json", "name,url"])
     
     return json.loads(res.stdout) 
 
 def create_dest_dir(dest_path):
     dest_path = Path(dest_path)
     dest_path.mkdir(parents=True, exist_ok=True)
-    count = len(os.listdir(dest_path))
+    count = max(1, len(os.listdir(dest_path)))
     dest_path /= f'backup_{count}_{datetime.now().strftime('%Y-%m-%d_%Hh_%Mm_%Ss')}'
     dest_path.mkdir(parents=True, exist_ok=True)
     
@@ -87,10 +87,27 @@ def clone_repos(repos, dest_path):
     #     print(r['url'])
     #     subprocess.run(["git", "clone", r['url']], cwd=dest_path, shell=True)
 
+def compress_dest(dest_path):
+    dest_path = Path(dest_path)
+    arc_path = dest_path / '..' / 'archive'
+    print(arc_path)
+    arc_path.mkdir(parents=True, exist_ok=True)
+    dest_path = arc_path / dest_path.parts[-1]
+    shutil.make_archive(dest_path, 'zip', arc_path)
+
+def remove_uncompressed_dest(dest_path):
+    try:
+        shutil.rmtree(dest_path)
+    except PermissionError as e:
+        print(f"PermissionError: {e}")
+
 # By default all public and private repos are backed up, but not forks
-# options:
+# TODO:
+# 1. options:
 #     * clone forks
 #     * clone submodules
+# 2. browsing an archive in a (simple gh-like) UI
+# 3. resyncing (but should it push/pull or make a new backup etc.)
 def main():
     dest_path = './gh_backup'
 
@@ -102,7 +119,17 @@ def main():
     repos = repo_list()
     dest_path = create_dest_dir(dest_path)
 
+    print('Cloning Repositories...')
     clone_repos(repos, dest_path)
+    print('Finished Cloning Repositories...')
+
+    print('Compressing Result...')
+    compress_dest(dest_path)
+
+    # print('Removing Uncompressed Result...')
+    # remove_uncompressed_dest(dest_path)
+    print('Done...')
+
 
 
 if __name__ == '__main__':
